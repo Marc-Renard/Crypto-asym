29b1dd0ad6a3782c36714e2ff4de51ecf14a6af4
Marc Renard fixe <renard.marc1@hotmail.fr>
2022-01-19 17:45:09 +0100

    Création tableau S

--- crible_quadra/crible.c
+++ crible_quadra/crible.c
@@ -15,11 +15,20 @@ int main(int argc, char **argv){
     mpz_set_str(n, argv[2], 10);
 
     mpz_t B[p];
+    mpz_init_table(B, p);
     mpz_fill_table_B(B, p, n);
-    mpz_print_table_B(B, p);
-    mpz_free_table_B(B, p);
+    mpz_print_table(B, p);
+    mpz_free_table(B, p);
+    
+
+    mpz_t S[10];
+    mpz_init_table(S,10);
+    mpz_fill_table_S(S, 10, n);
+    mpz_print_table(S, 10);
+    mpz_free_table(S,10);
 
     mpz_clear(n);
+
     return 0;
 }
 
@@ -30,7 +39,6 @@ void mpz_fill_table_B(mpz_t *tab, int64_t size, mpz_t n){
     mpz_inits(prime, tmp, NULL);
     mpz_set_ui(prime, 2);
     for( int64_t cmptr = 1; cmptr < size; cmptr++ ){
-        mpz_init(tab[cmptr]);
         int test = 0;
         while( test != 1){
             mpz_nextprime(tmp, prime );
@@ -40,10 +48,13 @@ void mpz_fill_table_B(mpz_t *tab, int64_t size, mpz_t n){
         mpz_set(tab[cmptr], prime);        
     }
     mpz_clears(prime, tmp, NULL);
+
     return;
 }
 
-void mpz_print_table_B(mpz_t *tab, int64_t size){
+
+
+void mpz_print_table(mpz_t *tab, int64_t size){
     mpz_out_str(stdout, 10, tab [0]);
     for( int64_t i = 1; i < size ; i++){
         fprintf(stdout, " | ");
@@ -53,9 +64,33 @@ void mpz_print_table_B(mpz_t *tab, int64_t size){
     return;
 }
 
-void mpz_free_table_B(mpz_t *tab, int64_t size){
+void mpz_init_table(mpz_t *tab, int64_t size){
     for(int64_t i = 0 ; i < size ; i++){
-        mpz_clear(tab[i]);
+        mpz_init_set_ui(tab[i],0);
     }
+    return;
+}
+
+void mpz_free_table(mpz_t *tab, int64_t size){
+    for(int64_t i = 0 ; i < size ; i++){
+        mpz_init_set_ui(tab[i],0);
+    }
+    return;
+}
+
+void mpz_fill_table_S(mpz_t *tab, int64_t size_A, mpz_t n){
+    mpz_t root,tmp, tmp2;
+    mpz_inits(root, tmp, tmp2, NULL);
+    mpz_sqrt(root, n);
+
+    for( int64_t cmptr = 1; cmptr <= size_A; cmptr++ ){
+
+        mpz_add_ui(tmp,root, cmptr);
+
+        mpz_pow_ui(tmp2, tmp, 2);
+
+        mpz_sub(tab[cmptr-1], tmp2, n);
+    }
+    mpz_clears(root, tmp, tmp2, NULL);
     return;
 }
--- crible_quadra/crible.h
+++ crible_quadra/crible.h
@@ -3,8 +3,12 @@
 
 void mpz_fill_table_B(mpz_t *tab, int64_t size, mpz_t n);
 
-void mpz_print_table_B(mpz_t *tab, int64_t size);
+void mpz_print_table(mpz_t *tab, int64_t size);
 
-void mpz_free_table_B(mpz_t *tab, int64_t size);
+void mpz_free_table(mpz_t *tab, int64_t size);
+
+void mpz_init_table(mpz_t *tab, int64_t size);
+
+void mpz_fill_table_S(mpz_t *tab, int64_t size_A, mpz_t n);
 
 #endif /*CRIBLE_H*/
